cdad492d9943b83787b8a42b9aa41ae977598c42
shaddarko <shaddarkoz@gmail.com>
2020-09-10 18:05:23 +0300

    Fixed

--- src/test/java/HomeTask3.java
+++ src/test/java/HomeTask3.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -17,42 +18,56 @@ import java.util.concurrent.TimeUnit;
 
 public class HomeTask3 {
 
-    protected static WebDriver driver;
+
+    private WebDriver driver;
     private Logger logger = LogManager.getLogger(HomeTask3.class);
+
+    public WebElement waitUntilClickable(String xPathLocator) {
+
+        return new WebDriverWait(driver, 60).until(ExpectedConditions.elementToBeClickable(new By.ByXPath(xPathLocator)));
+    }
 
 
     @BeforeTest
     public void SetUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
         logger.info("Initializing webdriver...");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        logger.info("Driver initialized...");
 
 
     }
 
     @Test
     public void yandex() throws InterruptedException {
-        driver.get("https://market.yandex.ru/catalog--mobilnye-telefony/54726/list?hid=91491&onstock=1");
+
+
         logger.info("Opening target page");
-        new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(new By.ByXPath("//span[contains(text(),'HONOR')]"))).click();
-        driver.findElement(By.xpath("//span[contains(text(),'Xiaomi')]")).click();
-        driver.findElement(By.xpath("//button[@data-autotest-id='dprice']")).click();
+        driver.get("https://market.yandex.ru/catalog--mobilnye-telefony/54726/list?hid=91491&onstock=1");
+        logger.info("Target page opened");
+        waitUntilClickable("//span[contains(text(),'HONOR')]").click();
+        waitUntilClickable("//span[contains(text(),'Xiaomi')]").click();
+        waitUntilClickable("//button[@data-autotest-id='dprice']").click();
         Thread.sleep(4000);
         driver.findElement(By.xpath("//span[contains(text(),'Redmi')]/ancestor::article//div[contains(@aria-label,'сравнению')]")).click();
         driver.findElement(By.xpath("//div[contains(text(),'HONOR')]/ancestor::article//div[contains(@aria-label,'сравнению')]")).click();
-        new WebDriverWait(driver, 1).until(ExpectedConditions.elementToBeClickable(new By.ByXPath("//span[contains(text(),'Сравнить')]"))).click();
-        new WebDriverWait(driver, 1).until(ExpectedConditions.elementToBeClickable(new By.ByXPath("//button[contains(text(),'Все характеристики')]"))).click();
-        new WebDriverWait(driver, 1).until(ExpectedConditions.elementToBeClickable(new By.ByXPath("//div[contains(text(),'Операционная система')]")));
-        new WebDriverWait(driver, 1).until(ExpectedConditions.elementToBeClickable(new By.ByXPath("//button[contains(text(),'Различающиеся характеристики')]"))).click();
+        waitUntilClickable("//span[contains(text(),'Сравнить')]").click();
+        waitUntilClickable("//button[contains(text(),'Все характеристики')]").click();
+        waitUntilClickable("//div[contains(text(),'Операционная система')]");
+        waitUntilClickable("//button[contains(text(),'Различающиеся характеристики')]").click();
         Boolean isPresent = driver.findElements(By.xpath("//div[contains(text(),'Операционная система')]")).size() > 0;
+
 
     }
 
     @AfterTest
     public void tearDown() {
-        System.out.println();
-        driver.close();
-        logger.info("Closing driver...");
+        if (driver != null) {
+            logger.info("Closing driver...");
+            driver.close();
+            logger.info("Driver closed...");
+        }
 
     }
 }
